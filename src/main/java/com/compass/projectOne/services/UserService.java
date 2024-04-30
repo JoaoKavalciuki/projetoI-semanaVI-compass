f65c4829a74c12fe27dc0428832ed780892493ab
João <joaokavalciuki@hotmail.com>
2024-04-30 15:18:49 -0300

    UserService e métdo findAll implmentados

---
+++ src/main/java/com/compass/projectOne/services/UserService.java
@@ -0,0 +1,23 @@
+package com.compass.projectOne.services;
+
+import com.compass.projectOne.entities.User;
+import com.compass.projectOne.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    private List<User>  findAll(){
+        return userRepository.findAll();
+    }
+}

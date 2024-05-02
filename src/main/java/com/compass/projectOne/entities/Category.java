f9688cd131d621f7fb52c88626bbfbf5cafc553f
João <joaokavalciuki@hotmail.com>
2024-05-02 15:55:01 -0300

    Função: construtor com name implementado

--- src/main/java/com/compass/projectOne/entities/Category.java
+++ src/main/java/com/compass/projectOne/entities/Category.java
@@ -16,6 +16,10 @@ public class Category implements Serializable {
     public Category() {
     }
 
+    public Category(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }

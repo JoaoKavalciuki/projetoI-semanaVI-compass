db63cc95a766838fbb6ce94c79a2839a5abd4741
João <joaokavalciuki@hotmail.com>
2024-05-03 13:20:31 -0300

    Função: Categorias mock implementadas

--- src/main/java/com/compass/projectOne/config/TestConfig.java
+++ src/main/java/com/compass/projectOne/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.compass.projectOne.config;
 
+import com.compass.projectOne.entities.Category;
 import com.compass.projectOne.entities.Order;
 import com.compass.projectOne.entities.User;
 import com.compass.projectOne.entities.enums.OrderStatus;
+import com.compass.projectOne.repositories.CategoryRepository;
 import com.compass.projectOne.repositories.OrderRepository;
 import com.compass.projectOne.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +21,13 @@ public class TestConfig implements CommandLineRunner {
     private UserRepository userRepository;
 
     private OrderRepository orderRepository;
+    private CategoryRepository categoryRepository;
 
     @Autowired
-    public TestConfig(UserRepository userRepository, OrderRepository orderRepository){
+    public TestConfig(UserRepository userRepository, OrderRepository orderRepository, CategoryRepository categoryRepository){
         this.userRepository = userRepository;
         this.orderRepository = orderRepository;
+        this.categoryRepository = categoryRepository;
     }
     @Override
     public void run(String... args) {
@@ -34,7 +38,12 @@ public class TestConfig implements CommandLineRunner {
         Order order2 = new Order(Instant.parse("2024-03-03T09:45:00Z"), user1, OrderStatus.SHIPPED);
         Order order3 = new Order(Instant.parse("2024-01-02T12:30:00Z"), user2, OrderStatus.WAITING_PAYMENT);
 
+        Category category = new Category("Shoes");
+        Category category2 = new Category("Electronic");
+        Category category3 = new Category("Cleaning");
+
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order, order2, order3));
+        categoryRepository.saveAll(Arrays.asList(category, category2, category3));
     }
 }

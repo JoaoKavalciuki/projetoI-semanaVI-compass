56096e9f4d1292e8b7b72ff5330eed13f1fa6a5e
João <joaokavalciuki@hotmail.com>
2024-05-03 14:03:02 -0300

    Função: Produtos mock adicionados

--- src/main/java/com/compass/projectOne/config/TestConfig.java
+++ src/main/java/com/compass/projectOne/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.compass.projectOne.config;
 
 import com.compass.projectOne.entities.Category;
 import com.compass.projectOne.entities.Order;
+import com.compass.projectOne.entities.Product;
 import com.compass.projectOne.entities.User;
 import com.compass.projectOne.entities.enums.OrderStatus;
 import com.compass.projectOne.repositories.CategoryRepository;
 import com.compass.projectOne.repositories.OrderRepository;
+import com.compass.projectOne.repositories.ProductRepository;
 import com.compass.projectOne.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,12 +24,16 @@ public class TestConfig implements CommandLineRunner {
 
     private OrderRepository orderRepository;
     private CategoryRepository categoryRepository;
+    private ProductRepository productRepository;
 
     @Autowired
-    public TestConfig(UserRepository userRepository, OrderRepository orderRepository, CategoryRepository categoryRepository){
+    public TestConfig(UserRepository userRepository, OrderRepository orderRepository, CategoryRepository categoryRepository,
+    ProductRepository productRepository)
+    {
         this.userRepository = userRepository;
         this.orderRepository = orderRepository;
         this.categoryRepository = categoryRepository;
+        this.productRepository = productRepository;
     }
     @Override
     public void run(String... args) {
@@ -42,8 +48,13 @@ public class TestConfig implements CommandLineRunner {
         Category category2 = new Category("Electronic");
         Category category3 = new Category("Cleaning");
 
+        Product product = new Product("Ryzen 5", "AMD CPU", "", 150.00);
+        Product product2 = new Product("Water cooler", "NZXT water cooler", "", 75.00);
+        Product product3 = new Product("MX Keys", "Logitech keyboard", "", 100.00);
+
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order, order2, order3));
         categoryRepository.saveAll(Arrays.asList(category, category2, category3));
+        productRepository.saveAll(Arrays.asList(product, product2, product3));
     }
 }

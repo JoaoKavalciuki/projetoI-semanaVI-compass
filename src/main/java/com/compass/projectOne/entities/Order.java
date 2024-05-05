d9dc5b683e6b6c207d7a35529661ff1e6c3ea07c
João <joaokavalciuki@hotmail.com>
2024-05-05 12:12:04 -0300

    Função: método para retornar o preço de uma compra

--- src/main/java/com/compass/projectOne/entities/Order.java
+++ src/main/java/com/compass/projectOne/entities/Order.java
@@ -77,6 +77,14 @@ public class Order implements Serializable {
     public Set<OrderItem> getItems() {
         return items;
     }
+
+    public Double getTotal(){
+        Double sum = 0.0;
+        for(OrderItem item: items){
+            sum += item.getSubTotal();
+        }
+        return  sum;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -84,6 +92,7 @@ public class Order implements Serializable {
         Order order = (Order) o;
         return Objects.equals(id, order.id);
     }
+
 
     @Override
     public int hashCode() {
